44195b36e75aefbd56bcb2bb0af9aaf298313a49
Stan Laura-Ioana <laurastan1999@gmail.com>
2020-04-07 17:18:01 +0300

    update

--- php/inregistrare.php
+++ php/inregistrare.php
@@ -1,18 +1,53 @@
 <?php
+$numeErr =$prenumeErr= $emailErr = $adresaErr = $telefonErr = $parolaErr = "";
+
+if(empty($_REQUEST["nume"])){
+  $numeErr = "Numele este necesar";
+}
+
+if(empty($_REQUEST["prenume"])){
+  $prenumeErr = "Prenumele este necesar";
+}
+
+if(empty($_REQUEST["email"])){
+  $emailErr = "Email-ul este necesar";
+}
+
+if(empty($_REQUEST["adresa"])){
+  $adresaErr = "Adresa este necesar";
+}
+
+if(empty($_REQUEST["telefon"])){
+  $telefonErr = "Telefon este necesar";
+}
+
+if(empty($_REQUEST["parola"])){
+  $parolaErr = "Parola este necesar";
+}
+
 
 $db = new mysqli('localhost:3308', 'user', '1234', 'impressed');
 
 if($db->connect_error){
     $message = $db->connect_error;
 }
-else{ echo"succes";
+else{
 
     $sql=$db->prepare("insert into utilizatori (nume,prenume,email,telefon,adresa,parola)
                         values (?,?,?,?,?,?)");
-    $sql->bind_param("ssssss",$_REQUEST["nume"],$_REQUEST["prenume"],$_REQUEST["email"],
-                          $_REQUEST["telefon"],$_REQUEST["adresa"],$_REQUEST["parola"]);
-    $sql->execute();
 
+    if($_REQUEST["nume"]==null ||$_REQUEST["prenume"]==null||$_REQUEST["email"]==null||
+       $_REQUEST["telefon"]==null||$_REQUEST["adresa"]==null||$_REQUEST["parola"]==null)
+       {
+        // echo" nu au fost completate toate sectiunile";
+       }
+       else{
+        
+            $sql->bind_param("ssssss",$_REQUEST["nume"],$_REQUEST["prenume"],$_REQUEST["email"],
+                          $_REQUEST["telefon"],$_REQUEST["adresa"],$_REQUEST["parola"]);
+            $sql->execute();
+            $db->close();}
+       }
+       
     
-    $db->close();}
 ?>
