3d89938eded6c61ba2d30a71f4ac5886313f4b5c
Georgiana-hub <botez.georgiana99@gmail.com>
2020-04-16 14:36:13 +0300

    Update footer.php

--- view/footer.php
+++ view/footer.php
@@ -1,70 +1,55 @@
 <!DOCTYPE html>
 <html lang="ro">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="../css/footer.css" rel="stylesheet" />
 </head>
-    <footer class="footer">
-      <div class="footer-stanga">
+<footer class="footer">
+    <div class="footer-stanga">
         <h1>
-          <img id="logo" src="../Poze/logo.png" alt="logo" />
-          <img id="logo-nume" src="../Poze/logo-nume.png" alt="logo-nume" />
+            <img id="logo" src="../Poze/logo.png" alt="logo" />
+            <img id="logo-nume" src="../Poze/logo-nume.png" alt="logo-nume" />
         </h1>
         <p class="footer-links">
-          <a href="./PaginaPrincipala.html">Acasa</a>
-          |
-          <a href="./Livrare.html">Livrare si Servicii</a>
-          |
-          <a href="./Contact.html">Contact</a>
+            <a href="./paginaPrincipala-view.php">Acasa</a>
+            |
+            <a href="./livrare-view.php">Livrare si Servicii</a>
+            |
+            <a href="./contact-view.php">Contact</a>
         </p>
         <p class="nume-companie">Impressed © 2020</p>
-      </div>
+    </div>
 
-      <div class="footer-centru">
+    <div class="footer-centru">
         <div class="locatie">
-          <a title="Locatie"><img src="../Poze/locatie-tw.png" alt="locatie" /></a>
-          <p>Strada General Henri Mathias Berthelot Nr. 16</p>
+            <a title="Locatie"><img src="../Poze/locatie-tw.png" alt="locatie" /></a>
+            <p>Strada General Henri Mathias Berthelot Nr. 16</p>
         </div>
 
         <div class="telefon">
-          <a title="Telefon"><img src="../Poze/telefon-tw.png" alt="telefon" /></a>
-          <p>+4 0767-437-025</p>
+            <a title="Telefon"><img src="../Poze/telefon-tw.png" alt="telefon" /></a>
+            <p>+4 0767-437-025</p>
         </div>
 
         <div class="mail">
-          <a
-            href="mailto:Impressed2020@company.com?Subject=Impressed"
-            title="Mail"
-            ><img src="../Poze/mail-tw.png" alt="mail"
-          /></a>
-          <p>Impressed@company.com</p>
+            <a href="mailto:Impressed2020@company.com?Subject=Impressed" title="Mail"><img src="../Poze/mail-tw.png" alt="mail" /></a>
+            <p>Impressed@company.com</p>
         </div>
-      </div>
+    </div>
 
-      <div class="footer-dreapta">
+    <div class="footer-dreapta">
         <p>
-          Urmărește-ne
+            Urmărește-ne
         </p>
         <div class="footer-icons">
-          <a title="Facebook"
-            ><img id="facebook" src="../Poze/facebook-tw.png" alt="facebook-icon"
-          /></a>
-          <a title="Instagram"
-            ><img
-              id="instagram"
-              src="../Poze/instagram-tw.png"
-              alt="instagram-icon"
-          /></a>
-          <a
-            href="https://github.com/LauraStan99/FII/tree/master/TW"
-            title="GitHub"
-            ><img id="git" src="../Poze/git-tw.png" alt="github-icon"
-          /></a>
-          <a title="YouTube"
-            ><img id="youtube" src="../Poze/youtube-tw.png" alt="youtube-icon"
-          /></a>
+            <a title="Facebook"><img id="facebook" src="../Poze/facebook-tw.png" alt="facebook-icon" /></a>
+            <a title="Instagram"><img id="instagram" src="../Poze/instagram-tw.png" alt="instagram-icon" /></a>
+            <a href="https://github.com/LauraStan99/FII/tree/master" title="GitHub"><img id="git" src="../Poze/git-tw.png" alt="github-icon" /></a>
+            <a title="YouTube"><img id="youtube" src="../Poze/youtube-tw.png" alt="youtube-icon" /></a>
         </div>
-      </div>
-    </footer>
+    </div>
+</footer>
+
 </html>
