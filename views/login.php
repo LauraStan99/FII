55293878b6acd4a8cbdcbea92e51b9b72a04dfae
Georgiana-hub <botez.georgiana99@gmail.com>
2020-04-24 19:34:58 +0300

    Update

--- views/login.php
+++ views/login.php
@@ -5,7 +5,7 @@
   <title>Logare | Impressed</title>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link href="<?php echo URL; ?>public/css/login2.css" rel="stylesheet" />
+  <link href="<?php echo URL; ?>public/css/login.css" rel="stylesheet" />
 </head>
 
 <body>
