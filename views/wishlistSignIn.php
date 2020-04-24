4c321e4a184dea50c46bba941f16888f818b3ba6
Stan Laura-Ioana <laurastan1999@gmail.com>
2020-04-24 18:59:19 +0300

    update

--- views/wishlistSignIn.php
+++ views/wishlistSignIn.php
@@ -5,7 +5,7 @@
   <title>Favorite | Impressed</title>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link href="<?php echo URL; ?>public/css/wishlistSignIn.css" rel="stylesheet" />
+  <link href="<?php echo URL; ?>public/css/wishlistSignIn1.css" rel="stylesheet" />
 </head>
 
 <body>
