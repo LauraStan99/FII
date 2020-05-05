4678b82c150277d653e5b64595eb74d301cd05e5
Stan Laura-Ioana <laurastan1999@gmail.com>
2020-05-05 12:53:29 +0300

    update

--- views/catalogFRochii.php
+++ views/catalogFRochii.php
@@ -5,7 +5,7 @@
     <title>Cont | Impressed</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="<?php echo URL; ?>public/css/catalogFRochii.css" rel="stylesheet" />
+    <link href="<?php echo URL; ?>public/css/catalogFRochii1.css" rel="stylesheet" />
 </head>
 
 <body>
@@ -17,11 +17,102 @@
     <main>
 
 
-        <?php
+        <div class="filter">
+            <div class="prices">
+                <h1>PRET</h1>
+                <div class="check-price">
+                    <label><input type="checkbox" />Pana la 50 Lei</label>
+                    <label><input type="checkbox" />50 - 100 Lei</label>
+                    <label><input type="checkbox" />100 - 150 Lei</label>
+                    <label><input type="checkbox" />150 - 200 Lei</label>
+                    <label><input type="checkbox" />200 - 250 Lei</label>
+                    <label><input type="checkbox" />Peste 250 Lei</label>
+                </div>
+            </div>
+            <hr>
+            <div class="size">
+                <h1>MARIME</h1>
+                <div class="check-size">
+                    <label><input type="checkbox" />XS/34</label>
+                    <label><input type="checkbox" />S/36</label>
+                    <label><input type="checkbox" />M/38</label>
+                    <label><input type="checkbox" />L/40</label>
+                    <label><input type="checkbox" />XL/42</label>
+                    <label><input type="checkbox" />XXL/44</label>
+                </div>
+            </div>
+            <hr>
+            <div class="colors">
+                <h1> CULOARE </h1>
+                <div class="icons-colors">
+                    <a> <img src="<?php echo URL; ?>public/poze/red.png" alt="red" /> </a>
+                    <a> <img src="<?php echo URL; ?>public/poze/yellow.png" alt="yellow" /></a>
+                    <a> <img src="<?php echo URL; ?>public/poze/blue.png" alt="blue" /></a>
+                    <a> <img src="<?php echo URL; ?>public/poze/black.png" alt="black" /></a>
+                    <a> <img src="<?php echo URL; ?>public/poze/white.png" alt="white" /> </a>
+                    <a> <img src="<?php echo URL; ?>public/poze/green.png" alt="green" /> </a>
+                </div>
 
-        while ($row = mysqli_fetch_array($this->result)) {
-        ?>
-            <div class="row">
+            </div>
+            <hr>
+            <div class="fabrics">
+                <h1>MATERIAL</h1>
+                <div class="check-fabrics">
+                    <label><input type="checkbox" /> bumbac</label>
+                    <label><input type="checkbox" /> matase</label>
+                    <label><input type="checkbox" /> sintetic</label>
+                    <label><input type="checkbox" /> catifea</label>
+                    <label><input type="checkbox" /> satin</label>
+                    <label><input type="checkbox" /> stofa</label>
+                    <label><input type="checkbox" /> blug</label>
+                </div>
+
+            </div>
+            <hr>
+            <div class="occasions">
+                <h1>OCAZIE</h1>
+                <div class="check-occasions">
+                    <label><input type="checkbox" /> casual</label>
+                    <label><input type="checkbox" /> sport</label>
+                    <label><input type="checkbox" /> bussines</label>
+                    <label><input type="checkbox" /> festival</label>
+                    <label><input type="checkbox" /> cocktail</label>
+                    <label><input type="checkbox" /> aniversare</label>
+                    <label><input type="checkbox" /> bal</label>
+                </div>
+
+            </div>
+            </br>
+
+            <button type="submit" class="aplica">Aplica</button>
+        </div>
+
+
+        <div class="products">
+
+            <h2>ROCHII</h2>
+            <hr>
+            <div class="sortare">
+                <p>Ordoneaza dupa: </p>
+
+                <?php
+
+                (isset($_POST['tip-ordonare'])) ? $ordonare = $_POST['tip-ordonare'] : $ordonare = 1;
+                ?>
+
+                <form>
+                    <select id="ordonare" name="tip-ordonare">
+                        <option <?php if ($ordonare == 1) echo 'a fost ales nr 1'; ?> value="1">Cele mai populare</option>
+                        <option <?php if ($ordonare == 2) echo 'a fost ales nr 2'; ?>value="2">Sortare alfabetica</option>
+                        <option <?php if ($ordonare == 3) echo 'a fost ales nr 3'; ?>value="3">Pret crescator</option>
+                        <option <?php if ($ordonare == 4) echo 'a fost ales nr 4'; ?>value="4">Pret descrescator</option>
+                    </select>
+
+                </form>
+            </div>
+            <?php
+            while ($row = mysqli_fetch_array($this->result)) {
+            ?>
                 <div class="column">
                     <div class="card">
                         <img src="<?php echo URL; ?>public/poze/<?php echo  $row['imagine']; ?>.png" alt="Denim Jeans" style="width:70% ; height:70%;">
@@ -32,6 +123,7 @@
                     </div>
                 </div>
             <?php  } ?>
+        </div>
 
 
     </main>
