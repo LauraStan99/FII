4dcfdb33c15d7a508db5b8edf4630555264f7da1
Stan Laura-Ioana <laurastan1999@gmail.com>
2020-05-06 16:21:40 +0300

    productPage

---
+++ views/productPage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ro">
+
+<head>
+    <title>Detalii produs | Impressed</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="../public/css/product.css" rel="stylesheet" />
+</head>
+
+<body>
+
+    <main>
+        <div class="imagine-produs">
+            <img id="imagine" src="../public/poze/c-fuste10.png">
+        </div>
+        <div class="detalii">
+
+            <h1>Fusta vaporoasa</h1>
+            <p>Pret : 70 lei</p>
+            <p>Culoare : rosu </p>
+            <p>Marime : ... </p>
+            <div class="butoane-marime">
+                <button id="buton">XS/34</button>
+                <button id="buton">S/36</button>
+                <button id="buton">M/38</button>
+                <button id="buton">L/40</button>
+                <button id="buton">XL/42</button>
+                <button id="buton">XXL/44</button>
+            </div>
+            </br>
+
+            <button type="submit" id="adauga-cos">Adauga in cos</button>
+            <button type="submit" id="adauga-favorite">Adauga la favorite </button>
+
+        </div>
+        <div class="descriere-produs">
+            <h1>Descriere</h1>
+            <p>Fustă midi confortabilă confecționată din material din bumbac ușor elastic cu imprimeu floral colorat. Are fason cloș, cu talia înaltă evidențiată cu cordon cu cataramă în talie. Fusta se încheie cu fermoar la spate.
+                Modelul din imagine are 177 cm înălțime și prezintă mărimea 36. Lungimea fustei pentru această mărime este de 65 cm.</p>
+            </br>
+            <h1>Material și îngrijire</h1>
+            <p>Material : bumbac</p>
+        </div>
+    </main>
+
+    <?php
+    require 'footer.php';
+    ?>
+</body>
+
+</html>
