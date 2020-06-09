830c6d37bfdaaea1edee843bafced9f216ba70e6
Stan Laura-Ioana <laurastan1999@gmail.com>
2020-06-09 18:02:57 +0300

    controllere comentate

--- models/cart_model.php
+++ models/cart_model.php
@@ -6,33 +6,42 @@ class Cart_model extends Model
         parent::__construct();
     }
 
+    /**
+     * functia returneaza produsele aflate in cosul de cumparaturi al unui utilizator logat , al carui id este preluat din sesiune
+     */
     public function selectCartProducts()
     {
-        if (!isset($_SESSION))
-        {
+        if (!isset($_SESSION)) {
             session_start();
         }
         $id = Session::get('id_utilizator');
         return $result = $this->db->selectJoin('cos', 'produse', 'id_produs', 'id_produs', 'id_utilizator', $id);
     }
 
-    public function selectCartTotalPrice(){
-        if (!isset($_SESSION))
-        {
+    /**
+     * pentru un utilizator logat , al carui id este preluat din sesiune se face totalul cosului de cumparaturi din sesiune
+     */
+    public function selectCartTotalPrice()
+    {
+        if (!isset($_SESSION)) {
             session_start();
         }
         $id = Session::get('id_utilizator');
         $result = $this->db->select1('cos', 'id_utilizator', $id);
         $total = 0;
-        while($row = $result->fetch()){
+        while ($row = $result->fetch()) {
             $total = $total + $row['pretTotal'];
         }
         return $total;
     }
 
-    public function addToCommand(){
-        if (!isset($_SESSION))
-        {
+    /**
+     * in momentul in care este actionat butonul de plasare a comenzii toate produsele din cos sunt adaugate in tabela "produse_comanda" 
+     * ordonate dupa id descrescator, stabilind astfel si id-ul posibilei comenzi respective
+     */
+    public function addToCommand()
+    {
+        if (!isset($_SESSION)) {
             session_start();
         }
         $id = Session::get('id_utilizator');
@@ -40,7 +49,7 @@ class Cart_model extends Model
         $result_id_command = $this->db->selectOrderBy('produse_comanda', 'id_comanda', 'desc');
         $row_id_command = $result_id_command->fetch();
         $id_command = $row_id_command['id_comanda'] + 1;
-        while($row = $result->fetch()){
+        while ($row = $result->fetch()) {
             $insert_data = array(
                 'id_comanda' => $id_command, 'id_produs' => $row['id_produs'], 'cantitate' => $row['cantitate'], 'pret' => $row['pretTotal'], 'id_utilizator' => $id, 'marime' => $row['marime']
             );
@@ -48,14 +57,18 @@ class Cart_model extends Model
         }
     }
 
-    public function selectIdComanda(){
-        if (!isset($_SESSION))
-        {
+    /**
+     * functia returneaza pentru un utilizator logat , al carui id este preluat din sesiune
+     * id-ul celei mai recente comenzi realizate de acesta
+     */
+    public function selectIdComanda()
+    {
+        if (!isset($_SESSION)) {
             session_start();
         }
         $id = Session::get('id_utilizator');
         $result = $this->db->selectOrderBy1('produse_comanda', 'id_utilizator', $id, 'id_comanda', 'desc');
-        $row = $result -> fetch();
+        $row = $result->fetch();
         return $row['id_comanda'];
     }
 }
--- models/cauta_model.php
+++ models/cauta_model.php
@@ -1,65 +1,86 @@
 <?php
 
-class Cauta_model extends Model{
+class Cauta_model extends Model
+{
 
     public function __construct()
     {
         parent::__construct();
     }
 
-
-    public function selectSearch($start_from, $limit){
+    /**
+     * functia returneaza produsele din tabela "produse_filter_order"  in limita permisa avand praguri de returnare $start_from si  $limit 
+     */
+    public function selectSearch($start_from, $limit)
+    {
 
         return $this->db->selectLimit('produse_filter_order', $start_from, $limit);
     }
 
-    public function count(){
+    /**
+     * functia returneaza numarul de produse existente in tabela "produse_filter_order"
+     */
+    public function count()
+    {
         $result = $this->db->selectCountSimple('produse_filter_order');
         $row = $result->fetch();
         return $row['count(*)'];
     }
 
-    public function searchByWord($word, $start_from, $limit){
+    /**
+     * functia returneaza, in limita permisa avand praguri de returnare $start_from si  $limit ,
+     * produsele din baza de date in functie de cuvantul cheie introdus de catre utilizator in searchbar
+     */
+    public function searchByWord($word, $start_from, $limit)
+    {
         $this->deleteFromProduse_filter_order();
-        $result =  $this->db->selectSearch6('produse',$word, 'nume', 'material', 'descriere', 'tip', 'culoare', 'categorie');
-        while($row = $result->fetch()){
+        $result =  $this->db->selectSearch6('produse', $word, 'nume', 'material', 'descriere', 'tip', 'culoare', 'categorie');
+        while ($row = $result->fetch()) {
             $insert_data = array(
-                'id_produs' => $row['id_produs'], 'nume' => $row['nume'], 'pret' => $row['pret'], 'material' => $row['material'], 'imagine'=>$row['imagine'], 'descriere' => $row['descriere'],
+                'id_produs' => $row['id_produs'], 'nume' => $row['nume'], 'pret' => $row['pret'], 'material' => $row['material'], 'imagine' => $row['imagine'], 'descriere' => $row['descriere'],
                 'gen' => $row['gen'], 'tip' => $row['tip'], 'categorie' => $row['categorie'], 'culoare' => $row['culoare'], 'nr_accesari' => $row['nr_accesari']
             );
             $this->db->insert('produse_filter_order', $insert_data);
         }
-        return $this->db->selectSearch6Limit('produse',$word, 'nume', 'material', 'descriere', 'tip', 'culoare', 'categorie', $start_from, $limit);
+        return $this->db->selectSearch6Limit('produse', $word, 'nume', 'material', 'descriere', 'tip', 'culoare', 'categorie', $start_from, $limit);
     }
 
-    public function countSearch($word){
-        $result = $this->db->selectSearch6('produse',$word, 'nume', 'material', 'descriere', 'tip', 'culoare', 'categorie');
+    /**
+     * functia returneaza numarul de produse gasite in baza de date in functie de cuvantul cheie introdul de catre utilizator in searchbar
+     */
+    public function countSearch($word)
+    {
+        $result = $this->db->selectSearch6('produse', $word, 'nume', 'material', 'descriere', 'tip', 'culoare', 'categorie');
         $i = 0;
 
-        while($result->fetch()){
-            $i=$i+1;
+        while ($result->fetch()) {
+            $i = $i + 1;
         }
         return $i;
     }
 
-    public function sortare($filter,$order, $start_from, $limit)
-    { 
-    
-        $result=$this->db->selectOrderBy('produse_filter_order',$filter,$order);
+    /**
+     * functia returneaza produsele ordonate dupa coloana si filterul oferit ca si parametrii , avand o limita , pornind de la pragul $start_from
+     * pana la pargul $limit , aceste praguri egasindu-se printre parametrii functiei 
+     */
+    public function sortare($filter, $order, $start_from, $limit)
+    {
+
+        $result = $this->db->selectOrderBy('produse_filter_order', $filter, $order);
         $this->deleteFromProduse_filter_order();
-        while($row=$result->fetch())
-        {
+        while ($row = $result->fetch()) {
             $insert_data = array(
-                'id_produs' => $row['id_produs'], 'nume' => $row['nume'], 'pret' => $row['pret'], 'material' => $row['material'], 'imagine'=>$row['imagine'], 'descriere' => $row['descriere'],
+                'id_produs' => $row['id_produs'], 'nume' => $row['nume'], 'pret' => $row['pret'], 'material' => $row['material'], 'imagine' => $row['imagine'], 'descriere' => $row['descriere'],
                 'gen' => $row['gen'], 'tip' => $row['tip'], 'categorie' => $row['categorie'], 'culoare' => $row['culoare'], 'nr_accesari' => $row['nr_accesari']
             );
             $this->db->insert('produse_filter_order', $insert_data);
         }
-        return $this->db->selectOrderByLimit2('produse_filter_order',$filter,$order,  $start_from, $limit);
-    
-
+        return $this->db->selectOrderByLimit2('produse_filter_order', $filter, $order,  $start_from, $limit);
     }
 
+    /**
+     * functia face un delete asupra tabelei "produse_filter_order" , stergand toate inregistrarie din ea
+     */
     public function deleteFromProduse_filter_order()
     {
         return $this->db->deleteAll('produse_filter_order');
